e92a685f2b75fb4504ee4039827b6ac44b4e05c0
Joseph Boudou <jogo@matabio.net>
2013-08-21 13:45:21 +0200

    Implement SimpleBraid.addPending
    And change SimpleBraid's architecture

--- src/main/scala/at/logic/skeptik/algorithm/compressor/middleLower/SimpleBraid.scala
+++ src/main/scala/at/logic/skeptik/algorithm/compressor/middleLower/SimpleBraid.scala
@@ -13,21 +13,49 @@ import collection.immutable.{ HashMap => IMap }
 
 case class SBThread(val subproof: SequentProofNode, val fraction: Rational) {
   def /(divisor: Int) = SBThread(subproof, fraction/divisor)
+  def +(other: SBThread) =
+    if (subproof eq other.subproof)
+      SBThread(subproof, fraction + other.fraction)
+    else
+      throw new Exception("Adding different threads")
 }
 
 case class SimpleBraid(
-  val main:    Option[SBThread],
+  val main:    Option[SequentProofNode],
   val pending: Map[Either[E,E], SBThread],
-  val merged:  Seq[SBThread]
+  val merged:  Map[SequentProofNode, Rational]
 ) extends ProofBraid[SimpleBraid] {
+
+  def addPending(arg: (Either[E,E], SBThread)) = {
+    val (pivot, thread @ SBThread(subproof, fraction)) = arg
+    if (merged contains subproof) {
+      val nfrac = merged(subproof) + fraction
+      if (nfrac < 1)
+        SimpleBraid(main, pending, merged + (subproof -> nfrac))
+      else
+        SimpleBraid(main, pending, merged - subproof)
+    }
+    else if (pending contains pivot) {
+      val nThread = pending(pivot) + thread
+      if (nThread.fraction < 1)
+        SimpleBraid(main, pending + (pivot -> nThread), merged)
+      else (main, pivot) match {
+        case (None, _)           => SimpleBraid(Some(subproof),          pending - pivot, merged)
+        case (Some(s), Left(p))  => SimpleBraid(Some(R(subproof, s, p)), pending - pivot, merged)
+        case (Some(s), Right(p)) => SimpleBraid(Some(R(s, subproof, p)), pending - pivot, merged)
+      }
+    } else
+      SimpleBraid(main, pending + (pivot -> thread), merged)
+  }
 
   def resolveWith(other: SimpleBraid, resolution: R):SimpleBraid = (this,other) match {
 
     // Less than 2 main thread
-    // TODO
+    case (SimpleBraid(None,_,m), _) if (m.isEmpty) => (other /: pending)       { _ addPending _ }
+    case (_, SimpleBraid(None,_,m)) if (m.isEmpty) => (this  /: other.pending) { _ addPending _ }
 
-    // 
-    case _ => throw new Exception("Unhandled situation")
+    // Catch all
+    case _ => throw new Exception("Unhandled situation "+this+" "+other)
   }
   
   def divise(divisor: Int, pivot: Either[E,E]) = 
@@ -37,20 +65,22 @@ case class SimpleBraid(
         None,
         main match {
           case None => pendingDivided
-          case Some(thread) => pendingDivided + (pivot -> (thread / divisor))
+          case Some(subproof) => pendingDivided + (pivot -> SBThread(subproof, Rational.one / divisor))
         },
-        merged map {_ / divisor}
+        merged mapValues {_ / divisor}
       )
     }
 
   def finalMerge = main match {
-    case Some(SBThread(subproof,Rational.one)) => subproof
+    case Some(subproof) => subproof
     case _ => throw new Exception("Root node doesn't have all threads")
   }
+
+  override def toString(): String = "(" + main.size + "," + pending.size + "," + merged.size + ")"
 }
 
 object r {
-  implicit def initBraid(node: SequentProofNode) = SimpleBraid(Some(SBThread(node, Rational.one)), IMap(), Seq())
+  implicit def initBraid(node: SequentProofNode) = SimpleBraid(Some(node), IMap(), IMap())
 }
 import r._
 
